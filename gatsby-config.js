59bcc323b526c1a40a9ab70eba79c1c74debf781
hello@elvisguillen.com <elvisguillen@Elviss-MacBook-Pro.local>
2018-06-18 16:49:18 -0400

    Added Google Analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-121064157-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      },
+    }
   ]
 }
